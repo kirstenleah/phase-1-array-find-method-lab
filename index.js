6b2733962763db80281ce145b8ad25179137dcbf
Kirsten <kirsten.staggs@gmail.com>
2022-04-07 11:34:47 -0600

    commit tree b56edbc88ef08621e0888bee6c9698d7a16917d1

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // code your solution here
+
+// function loopAndFind(result, finder){
+//     for(let item of result){
+//       if(finder(item) === true){
+//         outcome = item
+//         break
+//       }
+//     }
+//     return outcome
+//   }
+//   function superbowlWin(win){
+//     if(win.result === 'W'){
+//     console.log(superbowlWin)
+//     return win.year
+//     }
+//   }
+
+// function superbowlWin(record){
+//     let answer = record.find(function(game) {
+//         return game.result === 'W';
+//     });
+//     if (answer)return answer.year
+// }
+
+function loopAndFind(arr, finder){
+    let answer;
+    for(let obj of arr) {
+        if(finder(obj)) {
+            answer = obj;
+            break;
+        }
+    }
+    return answer;
+  }
+function superbowlWin(arr) {
+    let answer = loopAndFind(arr, function(obj) {
+    return obj.result === 'W';
+});
+if (answer){
+    return answer.year;
+}
+}
+//   if(win.result === 'W'){
+//         console.log(superbowlWin)
+//         return win.year
